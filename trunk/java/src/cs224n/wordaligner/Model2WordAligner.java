0ce211dc60a59fef2694b4dca4bab06c00527865
mgummelt1 <mgummelt1@ad77d0aa-31c6-11de-ba44-d58fe8a9ce33>
2009-04-29 01:53:47 +0000

    all

--- trunk/java/src/cs224n/wordaligner/Model2WordAligner.java
+++ trunk/java/src/cs224n/wordaligner/Model2WordAligner.java
@@ -1,5 +1,4 @@
 package cs224n.wordaligner;  
-
 import cs224n.util.*;
 import java.util.List;
 import java.util.ArrayList;
@@ -14,37 +13,43 @@ public class Model2WordAligner extends WordAligner {
     private Counter<String> englishCount;
     private Counter<String> frenchCount;
     private Counter<Integer> dparam;
-    private String NULL_STRING = "<NULL>";
+    private int LOW = -100;
+    private int HIGH = 100;
+    private double NULL_PROB = .2;
 
     
     public Alignment alignSentencePair(SentencePair sentencePair) {
 	Alignment alignment = new Alignment();
 	
 	List<String> englishWords = new ArrayList<String>(sentencePair.getEnglishWords());
-	englishWords.add(0, NULL_STRING);
+	englishWords.add(0, NULL_WORD);
 	List<String> frenchWords = sentencePair.getFrenchWords();
 
-	int numFrenchWords = frenchWords.size();
-	int numEnglishWords = englishWords.size();
+	int I = frenchWords.size();
+	int J = englishWords.size();
 
-	for (int frenchPosition = 0; frenchPosition < numFrenchWords; frenchPosition++) {
-	    int englishPosition = -1;
+	for (int i = 0; i < I; i++) {
+	    String fre = frenchWords.get(i);
+
+	    int bestj = -1;
 	    double bestProb = -1;
-	    for(int posEngPosition = 0; posEngPosition < numEnglishWords; posEngPosition++){
-		double mult = 1;
-		mult = (posEngPosition == 0 ? .2 : .8 / numEnglishWords);
-		double prob = mult * alignVal(englishWords.get(posEngPosition), 
-					      frenchWords.get(frenchPosition));
-		if(englishWords.get(posEngPosition).equals(frenchWords.get(frenchPosition)))
+	    for(int j = 0; j < J; j++){
+		String eng = englishWords.get(j);
+		double mult = posProb(i, j, I, J);
+		double prob = mult * alignVal(eng, fre);
+
+		tprob *= Math.pow(10, -Math.abs(eng.length() - fre.length()));
+
+		if(fre.equals(eng))
 		    prob = 1;
 
 		if(prob > bestProb){
-		    englishPosition = posEngPosition;
+		    bestj = j;
 		    bestProb = prob;
 		}
 	    }
 
-	    alignment.addAlignment(englishPosition-1, frenchPosition, true);
+	    alignment.addAlignment(bestj-1, i, true);
 	}
 	return alignment;
     }
@@ -62,6 +67,12 @@ public class Model2WordAligner extends WordAligner {
     public CounterMap<String,String> getProbSourceGivenTarget(){ 
 	return transProb;
     }
+
+    private double posProb(int i, int j, int I, int J){
+	if(j == 0)
+	    return NULL_PROB;
+	return dparam.getCount(bucket(i, j-1, I, J));
+    }
     
     public void train(List<SentencePair> trainingPairs_) {
 	//things to look at - giving NULL a different alignment prob
@@ -69,13 +80,14 @@ public class Model2WordAligner extends WordAligner {
 	transProb = new CounterMap<String,String>();
 	englishCount = new Counter<String>();
 	frenchCount = new Counter<String>();
+	dparam = new Counter<Integer>();
 	
 
 	List<SentencePair> trainingPairs = new ArrayList<SentencePair>();
 
 	for(SentencePair pair : trainingPairs_){
 	    List<String> engWords = new ArrayList<String>(pair.getEnglishWords());
-	    engWords.add(0, NULL_STRING);
+	    engWords.add(0, NULL_WORD);
 	    List<String> freWords = new ArrayList<String>(pair.getFrenchWords());
 	    trainingPairs.add(new SentencePair(0, "", engWords, freWords));
 	}
@@ -96,47 +108,44 @@ public class Model2WordAligner extends WordAligner {
 	}
 
 	//itialize dparam
-	for(int i = -5; i <= 5; i++)
-	    dparam.setCount(i, 1.0/11);
+	for(int i = LOW; i <= HIGH; i++)
+	    dparam.setCount(i, 1.0/(HIGH - LOW + 1));
 
 
 	int frenchSize = frenchCount.keySet().size();
 
-	for(int i = 0; i < 10; i++){
-	    System.out.println(i);
+	for(int ii = 0; ii < 10; ii++){
+	    System.out.println(ii);
 	    CounterMap<String, String> count = new CounterMap<String, String>();
-	    Counter<Integer> dcount = new CounterMap<String, String>();
+	    Counter<Integer> dcount = new Counter<Integer>();
 	    Counter<String> total = new Counter<String>();
-	    int dtotal = 0;
+	    double dtotal = 0;
 
 	    for(SentencePair pair: trainingPairs){
 		Counter<String> total_s = new Counter<String>();
 		List<String> engWords = pair.getEnglishWords();		
 		List<String> freWords = pair.getFrenchWords();
 		
-		int I = freWords.length();
-		int J = engWords.length();
+		int I = freWords.size();
+		int J = engWords.size();
 
 		for(int i = 0; i < I; i++){
 		    String fre = freWords.get(i);
-		    total_s.setCount(fre, 0.0);
 		    for(int j = 0; j < J; j++){
 			String eng = engWords.get(j);
-			total_s.incrementCount(fre, transHelper(i, eng, fre, frenchSize) * 
-					       dparam(bucket(i, j, I, J)));
+			total_s.incrementCount(fre, transHelper(ii, eng, fre, frenchSize) * 
+					       posProb(i, j, I, J));
 		    }
-		}
-		
-		for(int i = 0; i < I; i++){//String fre : freWords)
-		    String fre = freWords.get(i);
 		    for(int j = 0; j < J; j++){
 			String eng = engWords.get(j);
-			double c = transHelper(i, eng, fre, frenchSize) * 
-			    dparam(i, j, I, J) / total_s.getCount(fre);
+			double c = transHelper(ii, eng, fre, frenchSize) * 
+			    posProb(i, j, I, J) / total_s.getCount(fre);
 			count.incrementCount(eng, fre, c);
 			total.incrementCount(eng, c);
+			dcount.incrementCount(bucket(i, j, I, J), c);
+			dtotal += c;
 		    }
-		}			 
+		}
 	    }
 
 	    transProb = new CounterMap<String, String>();
@@ -145,12 +154,16 @@ public class Model2WordAligner extends WordAligner {
 		for(String fre : count.getCounter(eng).keySet())
 		    transProb.setCount(eng, fre, count.getCount(eng, fre) / engTotal);
 	    }
+	    for(int i = LOW; i <= HIGH; i++){
+		dparam.setCount(i, dcount.getCount(i) / dtotal);
+	    }
 	}	
     }    
 
     private int bucket(int i, int j, int I, int J){
 	double val = j - (double)i * J / I;
-	return Math.min(5, Math.max(-5, (int) val));
+	int ret = Math.min(HIGH, Math.max(LOW, (int) val));
+	return ret;
     }
 
     private double transHelper(int iter, String eng, String fre, int frenchSize){
